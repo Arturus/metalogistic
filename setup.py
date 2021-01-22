116f8e9f25f8befc7b420d880bda94ac76b64030
tadamcz <tmkadamcz@gmail.com>
2021-01-22 14:29:42 -0800

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="metalogistic",
-    version="0.0.2",
+    version="0.0.3",
     author="Thomas Adamczewski",
     author_email="tmkadamcz@gmail.com",
     description="A Python package for the metalogistic distribution. The metalogistic or metalog distribution is a highly flexible probability distribution that can be used to model data without traditional parameters.",
